a3a33c4a99c420b11dd7b291997950a873afb5dd
mehrshadina <mehrshadinaa@gmail.com>
2025-12-08 03:44:57 +0330

    email authentication added to project.

---
+++ web/authentication_backends.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+class EmailBackend(ModelBackend):
+    def authenticate(self, request, password=None, **kwargs):
+        email = kwargs.get('email')
+        try:
+            user = UserModel.objects.get(email=email)
+            if user.check_password(password):
+                return user
+        except UserModel.DoesNotExist:
+            return None
