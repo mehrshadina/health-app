73e7cf9041723fa24502a4c6d38783c18c8994c8
mehrshadina <mehrshadinaa@gmail.com>
2025-12-08 03:34:28 +0330

    bug fixed.

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from web.views import TokenInfoView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     #path('api/docs1/', SpectacularRedocView.as_view(url_name='schema'), name='redoc-ui'),
     path('api/', include('web.urls')),
